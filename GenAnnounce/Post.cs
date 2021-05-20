3ded73c2ffe8591f4be9dd2fbb4f8337ab327ab4
Lewis Watson <lewiswatson55@protonmail.com>
2021-05-20 16:29:00 +0100

    Initial Commit

---
+++ GenAnnounce/Post.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenAnnounce
+{
+    public class Post
+    {
+        // Attributes
+        private string _title;
+        private string _when;
+        private string _where;
+        private string _what;
+        private string _emoji;
+
+        // Constructor with all attributes
+        public Post(string title, string when, string where, string what, string emoji)
+        {
+            _title = title;
+            _when = when;
+            _where = where;
+            _what = what;
+            _emoji = emoji;
+        }
+
+        // Methods
+
+        public string GenerateTwt()
+        {
+            return $"**{_emoji} {_title} {_emoji}**\n\n📆When: {_when}\n❓Where: {_where}\n📍What: {_what}\n\n@everyone";
+        }
+
+        // GETTERS AND SETTERS
+        /// Title Getter
+        public string Title
+        {
+            get { return _title; }
+        }
+        /// When Getter
+        public string When
+        {
+            get { return _when; }
+        }
+        /// Where Getter
+        public string Where
+        {
+            get { return _where; }
+        }
+        /// What Getter
+        public string What
+        {
+            get { return _what; }
+        }
+        /// Emoji Getter
+        public string Emoji
+        {
+            get { return _emoji; }
+        }
+
+    }
+}
---
+++ GenAnnounceUI/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using GenAnnounce;
+
+namespace GenAnnounceUI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void twt_submit_click(object sender, RoutedEventArgs e)
+        {
+            GenAnnounce.Post newPost = new GenAnnounce.Post(twt_title.Text, twt_when.Text, twt_where.Text, twt_what.Text, twt_emoji.Text);
+            //string messageToShow = $"Title: {newPost.Title}\nWhere: {newPost.Where}\nWhen: {newPost.When}\nWhat: {newPost.What}\nEmoji: {newPost.Emoji}";
+            //MessageBox.Show(messageToShow, "Post Information");
+            twt_out.Text = newPost.GenerateTwt();
+        }
+
+        private void twt_clear_click(object sender, RoutedEventArgs e)
+        {
+
+            twt_out.Text = "";
+            //MessageBox.Show($"Output field has been cleared.", "Clear");
+
+        }
+    }
+}
